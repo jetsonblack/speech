4bb1bc2738473698abd309e128d72c1bbad7f498
jetsonblack <jetsondblack@gmail.com>
2026-02-28 20:28:01 -0500

    fix: fix previous merge, fixed admin index format

--- src/main/java/edu/iu/speech/config/SecurityConfig.java
+++ src/main/java/edu/iu/speech/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/", "/toc", "/tableofcontents", "/all", "/speech/**").permitAll()
                 .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
-                .requestMatchers("/logout/**")
+                .requestMatchers("/logout/**").permitAll()
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                 .requestMatchers("/search/**").authenticated()
                 .anyRequest().authenticated()
