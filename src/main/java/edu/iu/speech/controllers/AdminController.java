643bdec733b85d759ed77a123141ae4abefdfd1c
jetsonblack <jetsondblack@gmail.com>
2026-03-02 23:52:27 -0500

    fix: admin controller routing

--- src/main/java/edu/iu/speech/controllers/AdminController.java
+++ src/main/java/edu/iu/speech/controllers/AdminController.java
@@ -1,4 +1,4 @@
-package edu.iu.speech.controller;
+package edu.iu.speech.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
