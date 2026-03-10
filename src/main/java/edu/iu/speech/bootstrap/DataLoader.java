6bc8d6c805d71c0ee80b20ed6b4f511e1d3b3005
jetsonblack <jetsondblack@gmail.com>
2026-03-10 18:14:45 +0000

    update: update datloader with more

--- src/main/java/edu/iu/speech/bootstrap/DataLoader.java
+++ src/main/java/edu/iu/speech/bootstrap/DataLoader.java
@@ -52,5 +52,17 @@ public class DataLoader implements CommandLineRunner {
                                 "So, first of all, I give up",
                                 null,
                                 p3, c1));
+
+                speechRepository.save(new Speech(
+                                "New Version is Made",
+                                "NOW INDEX IS TOC, YIPEEE",
+                                null,
+                                p3, c1));
+
+                speechRepository.save(new Speech(
+                                "Lorem Ipsum",
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                                null,
+                                p3, c3));
         }
 }
